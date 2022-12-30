7ddb731357b40d16b0adc665eccb79c8900f4305
Sri Panyam <sri.panyam@gmail.com>
2022-12-30 01:31:39 -0800

    Catching invalid method names

--- tssnippets/src/client.ts
+++ tssnippets/src/client.ts
@@ -1,23 +1,27 @@
-import { credentials, Metadata } from '@grpc/grpc-js'
-import { SnippetServiceClient } from './snippets'
-import { promisify } from 'util'
+import { credentials, Metadata } from '@grpc/grpc-js';
+import { SnippetServiceClient } from './snippets';
+import { promisify } from 'util';
 
-export function NewClient (addr = 'localhost:7000'): SnippetServiceClient {
+export function NewClient(addr = 'localhost:7000'): SnippetServiceClient {
   return new SnippetServiceClient(addr, credentials.createInsecure(), {
     'grpc.keepalive_time_ms': 120000,
     'grpc.http2.min_time_between_pings_ms': 120000,
     'grpc.keepalive_timeout_ms': 20000,
     'grpc.http2.max_pings_without_data': 0,
-    'grpc.keepalive_permit_without_calls': 1
-  })
+    'grpc.keepalive_permit_without_calls': 1,
+  });
 }
 
-export async function call<ReqType, RespType> (
+export async function call<ReqType, RespType>(
   client: SnippetServiceClient,
   reqname: string,
   reqparam: ReqType,
-  md: Metadata = new Metadata()
+  md: Metadata = new Metadata(),
 ): Promise<RespType> {
-  const func = (client as any)[reqname].bind(client)
-  return await promisify<ReqType, Metadata, RespType>(func)(reqparam, md)
+  const func = (client as any)[reqname] || null;
+  if (func == null) {
+    return Promise.reject(new Error('Invalid method: ' + reqname));
+  }
+  const boundfunc = func.bind(client);
+  return await promisify<ReqType, Metadata, RespType>(boundfunc)(reqparam, md);
 }
