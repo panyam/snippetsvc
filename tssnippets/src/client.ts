9b02cf0115034757551b5a3da1734f8e7dcecb79
Sri Panyam <sri.panyam@gmail.com>
2022-12-29 23:49:07 -0800

    Trying a promisified version of the service client

--- tssnippets/src/client.ts
+++ tssnippets/src/client.ts
@@ -1,18 +1,25 @@
-import { credentials, Metadata, ServiceError } from '@grpc/grpc-js';
+import { credentials, Metadata } from '@grpc/grpc-js';
+import { SnippetServiceClient } from './snippets';
+import { promisify } from 'util';
 
 export default function NewClient(
   addr = 'localhost:7000',
-): Client<typeof SnippetServiceDefinition> {
-  const client = new GreeterClient(
-    'localhost:50051',
-    credentials.createInsecure(),
-    {
-      'grpc.keepalive_time_ms': 120000,
-      'grpc.http2.min_time_between_pings_ms': 120000,
-      'grpc.keepalive_timeout_ms': 20000,
-      'grpc.http2.max_pings_without_data': 0,
-      'grpc.keepalive_permit_without_calls': 1,
-    },
-  );
-  return client;
+): SnippetServiceClient {
+  return new SnippetServiceClient(addr, credentials.createInsecure(), {
+    'grpc.keepalive_time_ms': 120000,
+    'grpc.http2.min_time_between_pings_ms': 120000,
+    'grpc.keepalive_timeout_ms': 20000,
+    'grpc.http2.max_pings_without_data': 0,
+    'grpc.keepalive_permit_without_calls': 1,
+  });
+}
+
+export async function call<ReqType, RespType>(
+  client: SnippetServiceClient,
+  reqname: string,
+  reqparam: ReqType,
+  md: Metadata = new Metadata(),
+): Promise<RespType> {
+  const func = (client as any)[reqname].bind(client);
+  return await promisify<ReqType, Metadata, RespType>(func)(reqparam, md);
 }
